4456600db36803174146d7ec3810c7df0083bb2f
Ross M Karchner <ross@karchner.com>
2019-05-29 10:24:31 -0400

    This contains non-secret, but Ross-specific stuff

--- indieauth/go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-isort -rc --atomic auth
-black -t py36 .
-sam build
-sam package --s3-bucket ross-sam --output-template-file deploy.yml
-sam deploy --template-file deploy.yml  --stack-name sam-indieauth --capabilities CAPABILITY_IAM\
-    --parameter-overrides HostedZoneName=karchner.com  CognitoSubdomain=auth \
-    CertificateArn="arn:aws:acm:us-east-1:797438674243:certificate/22e6a31c-bb50-46ec-ab62-1dc56bece5dc"
